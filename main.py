3082dd25b6b2152946924080a3f01aee12f37615
guilhem0908 <l7guilhem@gmail.com>
2025-11-22 19:37:37 +0100

    add main.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-if __name__ == '__main__':
-    print('Hello World')
+from track_utils import load_track, compute_world_bounds
+from simulation.main_simulation import process_pygame
+
+def main():
+    csv_path = "tracks/belgium.csv"
+    cones = load_track(csv_path)
+    world_bounds = compute_world_bounds(cones)
+
+    process_pygame(cones, world_bounds, path=None)
+
+if __name__ == "__main__":
+    main()
